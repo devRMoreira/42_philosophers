8e5aa6e8ebb40bab5c6712afae5d383a20edcd18
Ricardo Moreira <devrmoreira@gmail.com>
2025-03-08 14:58:51 +0000

    is_digit check

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 13:50:13 by rimagalh          #+#    #+#             */
-/*   Updated: 2025/03/08 14:37:22 by rimagalh         ###   ########.fr       */
+/*   Updated: 2025/03/08 14:58:26 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,30 @@ static int validate(long *input)
 		return (0);
 	return (1);
 }
-static void init_struct(long *input, t_data *philo)
+static void init_struct(long *input, t_data *data)
 {
-	philo->total_philo = input[0];
-	philo->time_die = input[1];
-	philo->time_eat = input[2];
-	philo->time_sleep = input[3];
-	philo->amount_eat = input[4];
+	data->total_philo = input[0];
+	data->time_die = input[1];
+	data->time_eat = input[2];
+	data->time_sleep = input[3];
+	data->amount_eat = input[4];
 }
 
-int parse_input(char **argv, t_data *philo)
+static int is_num(char *str)
+{
+	int i;
+
+	i = 0;
+	while(i < ft_strlen(str))
+	{
+		if(!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int parse_input(char **argv, t_data *data)
 {
 	int i;
 	long *input;
@@ -51,7 +65,11 @@ int parse_input(char **argv, t_data *philo)
 		return (1);
 	ft_memset(input, 0, sizeof(long *) * 5);
 	while(++i < 5)
+	{
+		if(!is_num(argv[i]))
+			return (free(input), 0);
 		input[i - 1] = ft_atol(argv[i]);
+	}
 
 	if(argv[5])
 		input[4] = ft_atol(argv[5]);
@@ -59,7 +77,7 @@ int parse_input(char **argv, t_data *philo)
 	if(!validate(input))
 		return (free(input), 0);
 
-	init_struct(input, philo);
+	init_struct(input, data);
 	return (1);
 }
 
