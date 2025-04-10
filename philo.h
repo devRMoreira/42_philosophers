beec2777e124ab673fd59d159a9ba202f511480e
Ricardo Moreira <115899434+devRMoreira@users.noreply.github.com>
2025-04-10 03:01:41 +0100

    Silly little typo on valid_input

--- philo.h
+++ philo.h
@@ -58,6 +58,6 @@ void	*ft_routine(void *philosopher);
 void	ft_monitor_rout(t_data *data);
 void	ft_print(t_philo *philo, char *str);
 void	ft_usleep(size_t time_sleep);
-int		t_valid_input(int ac, char **av);
+int		ft_valid_input(int ac, char **av);
 
 #endif
